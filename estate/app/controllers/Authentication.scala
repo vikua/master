a66af3c271c905192480d302dfe8da154973b85a
VictorKovrizhkin <vik.kovrizhkin@gmail.com>
2013-09-22 17:31:22 +0300

    database tests

---
+++ estate/app/controllers/Authentication.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import play.api.mvc.Controller
+
+/**
+ *          Date: 22.09.13
+ *
+ *          take a look:
+ *          http://www.playframework.com/documentation/2.0.x/ScalaSecurity
+ */
+object Authentication extends Controller {
+
+}
