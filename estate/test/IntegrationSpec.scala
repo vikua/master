e6434a0100c2db977cb1522158220c74000c48bc
VictorKovrizhkin <vik.kovrizhkin@gmail.com>
2013-09-25 15:36:50 +0300

    login and security

--- estate/test/IntegrationSpec.scala
+++ estate/test/IntegrationSpec.scala
@@ -4,11 +4,14 @@ import org.specs2.mutable._
 
 import play.api.test._
 import play.api.test.Helpers._
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
 
 /**
  * add your integration spec here.
  * An integration test will fire up a whole play application in a real (or headless) browser
  */
+@RunWith(classOf[JUnitRunner])
 class IntegrationSpec extends Specification {
 
     "Application" should {
@@ -17,7 +20,7 @@ class IntegrationSpec extends Specification {
             running(TestServer(3333), HTMLUNIT) {
                 browser =>
                     browser.goTo("http://localhost:3333/")
-                    browser.pageSource must contain("Your new application is ready.")
+                    browser.pageSource must contain("Sign in")
 
             }
         }
