a785b6489f890fda37405190f4cdd7377c266e2b
vkovrizhkin <viktor.kovrizhkin@dataart.com>
2013-12-13 18:52:20 +0200

    evolution scripts

--- estate/app/controllers/Application.scala
+++ estate/app/controllers/Application.scala
@@ -3,7 +3,7 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-object Application extends Controller with Secured{
+object Application extends Controller with Secured {
   
   def index = isAuthenticated { username => request =>
     Ok(views.html.index("Your new application is ready."))
